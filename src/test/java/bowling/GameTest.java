5d7d44b679719954be770ac894d001e4919680a2
Pirboss <pierre.jeanmougin@univ-tlse3.fr>
2016-03-16 17:34:02 +0100

    ajout de couverture de tests

--- src/test/java/bowling/GameTest.java
+++ src/test/java/bowling/GameTest.java
@@ -1,5 +1,7 @@
 package bowling;
 
+import bowling.exception.InvalidGameException;
+import bowling.exception.InvalidTurnException;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,11 +13,30 @@ public class GameTest {
 
     @Test
     public void testGetScore() throws Exception {
-        ITurn t1 = new Turn(5,5);
-        ITurn tlast = new ExtendedTurn(0,0,0);
+        ITurn t1 = new Turn(5, 5);
+        ITurn tlast = new ExtendedTurn(0, 0, 0);
 
-        Game game = new Game(new ITurn[]{t1,t1,t1,t1,t1,t1,t1,t1,t1,tlast});
+        Game game = new Game(new ITurn[]{t1, t1, t1, t1, t1, t1, t1, t1, t1, tlast});
 
-        assertEquals(game.getTotalScore(),170);
+        assertEquals(game.getTotalScore(), 170);
+    }
+
+    @Test
+    public void testException() throws Exception {
+        try {
+            Turn t = new Turn(50, 3);
+        } catch (InvalidTurnException e) {
+            assertEquals(e instanceof InvalidTurnException, true);
+        }
+    }
+
+    @Test
+    public void testGameException() throws Exception{
+        try {
+            Game game = new Game(new ITurn[]{new Turn(5, 3)});
+        }
+        catch(Exception e){
+            assertEquals(e instanceof InvalidGameException, true);
+        }
     }
 }
