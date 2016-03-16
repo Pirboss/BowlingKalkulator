3c3f01c61850dc77704d45ed19405449d26d7d1f
Arthur <bellal.arthur@gmail.com>
2016-03-16 16:52:00 +0100

    Implementation of normal turn score calculation

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -7,22 +7,41 @@ import bowling.exception.InvalidGameException;
  */
 public class Game
 {
+    public static final int TURN_NUMBER = 10;
     private final ITurn[] mTurns;
 
     public Game(ITurn[] turns) throws InvalidGameException
     {
-        if(turns.length != 10)
+        if(turns.length != TURN_NUMBER || !(turns[TURN_NUMBER - 1] instanceof ExtendedTurn))
             throw new InvalidGameException();
         mTurns = turns;
     }
 
-    public int getScore()
+    public int getTotalScore()
     {
-        for(ITurn turn : mTurns)
+        int score = 0;
+        for(int i = 0;i < TURN_NUMBER;i++)
         {
+            if(mTurns[i] instanceof Turn)
+            {
+                Turn turn = (Turn) mTurns[i];
 
+                if(turn.isStrike())
+                {
+                    if(i < TURN_NUMBER - 1)
+                        score += turn.getScore() + mTurns[i+1].getScore() + mTurns[i+2].getScore();
+                }
+                else if(turn.isSpare())
+                {
+                    score += turn.getScore() + mTurns[i+1].getScore();
+                }
+                else
+                {
+                    score += turn.getScore();
+                }
+            }
         }
-        return 0;
+        return score;
     }
 
 }
